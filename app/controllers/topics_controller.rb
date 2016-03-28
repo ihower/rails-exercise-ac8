4fb7d7a7038dc91cc9509e928991e0e254419628
Wen-Tien Chang <ihower@gmail.com>
2016-03-28 10:39:58 +0800

    User can create topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,34 @@
 class TopicsController < ApplicationController
 
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
-    @topics = Topic.page( params[:page] )
+    @topics = Topic.order("id DESC").page( params[:page] )
   end
 
   def show
     @topic = Topic.find( params[:id] )
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new( topic_params )
+    @topic.user = current_user
+
+    if @topic.save
+      redirect_to topic_url(@topic)
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def topic_params
+    params.require(:topic).permit(:content, :subject)
   end
 
 end
