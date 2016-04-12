904c47d546bda77b9f8680e7fd2d0257c7d40630
Wen-Tien Chang <ihower@gmail.com>
2016-04-12 10:08:42 +0800

    person scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401032037) do
+ActiveRecord::Schema.define(version: 20160412015832) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 20160401032037) do
 
   add_index "likes", ["topic_id"], name: "index_likes_on_topic_id"
   add_index "likes", ["user_id"], name: "index_likes_on_user_id"
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.integer  "age"
+    t.date     "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "user_id"
