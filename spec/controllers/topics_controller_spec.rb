2998aa6f0eeaf903d87a973b63895af7a4d0efc4
Wen-Tien Chang <ihower@gmail.com>
2016-04-12 11:05:59 +0800

    Add controller spec example

---
+++ spec/controllers/topics_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe TopicsController, type: :controller do
+
+  before do
+    @user = User.create!( :email => "test@example.com", :password => "12345678")
+    sign_in @user
+  end
+
+  describe "POST #create" do
+    
+    it "should create topic failed" do
+      post :create, :topic => { :subject => "" }
+
+      expect( Topic.count ).to eq(0)
+
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
+    end
+
+    it "should create topic" do
+      post :create, :topic => { :subject => "FOOBAR" }
+
+      topic = Topic.last
+      expect( Topic.count ).to eq(1)
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to( topic_url(topic) )
+    end
+
+    it "should create topic (mock version)" do
+      fake_topic = double(:topic)
+      expect(Topic).to receive(:new).with( { :subject => "FOOBAR" } ).and_return(fake_topic)
+      expect(fake_topic).to receive(:"user=")
+      expect(fake_topic).to receive(:save).and_return(true)
+
+      post :create, :topic => { :subject => "FOOBAR" }
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to( topic_url(fake_topic) )
+    end
+  end
+
+end
